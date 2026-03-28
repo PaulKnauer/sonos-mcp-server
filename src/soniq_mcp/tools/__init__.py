f976038bbbd53343ee8217e8217b5a5523cf8406
Paul Knauer <paul.knauer@gmail.com>
2026-03-28 09:39:03 +0200

    Wire VolumeAdapter, VolumeService, and register_volume into register_all

--- src/soniq_mcp/tools/__init__.py
+++ src/soniq_mcp/tools/__init__.py
@@ -15,11 +15,18 @@ from soniq_mcp.config import SoniqConfig
 def register_all(app: FastMCP, config: SoniqConfig) -> None:
     """Register all available tools, respecting tools_disabled."""
     from soniq_mcp.adapters.discovery_adapter import DiscoveryAdapter
+    from soniq_mcp.adapters.volume_adapter import VolumeAdapter
     from soniq_mcp.services.room_service import RoomService
+    from soniq_mcp.services.volume_service import VolumeService
     from soniq_mcp.tools.setup_support import register as register_setup
     from soniq_mcp.tools.system import register as register_system
+    from soniq_mcp.tools.volume import register as register_volume
 
     register_setup(app, config)
 
     room_service = RoomService(DiscoveryAdapter())
     register_system(app, config, room_service)
+
+    volume_adapter = VolumeAdapter()
+    volume_service = VolumeService(room_service, volume_adapter, config)
+    register_volume(app, config, volume_service)
