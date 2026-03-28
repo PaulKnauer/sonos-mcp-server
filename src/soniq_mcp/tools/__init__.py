dfc43e031a2f6668c438d047fdca4bf70a729baa
Paul Knauer <paul.knauer@gmail.com>
2026-03-28 14:17:00 +0200

    Wire QueueService and register_queue into register_all

--- src/soniq_mcp/tools/__init__.py
+++ src/soniq_mcp/tools/__init__.py
@@ -17,10 +17,12 @@ def register_all(app: FastMCP, config: SoniqConfig) -> None:
     from soniq_mcp.adapters.discovery_adapter import DiscoveryAdapter
     from soniq_mcp.adapters.soco_adapter import SoCoAdapter
     from soniq_mcp.services.playback_service import PlaybackService
+    from soniq_mcp.services.queue_service import QueueService
     from soniq_mcp.services.room_service import RoomService
     from soniq_mcp.services.sonos_service import SonosService
     from soniq_mcp.services.volume_service import VolumeService
     from soniq_mcp.tools.playback import register as register_playback
+    from soniq_mcp.tools.queue import register as register_queue
     from soniq_mcp.tools.setup_support import register as register_setup
     from soniq_mcp.tools.system import register as register_system
     from soniq_mcp.tools.volume import register as register_volume
@@ -36,3 +38,6 @@ def register_all(app: FastMCP, config: SoniqConfig) -> None:
 
     volume_service = VolumeService(sonos_service=sonos_service)
     register_volume(app, config, volume_service)
+
+    queue_service = QueueService(room_service, SoCoAdapter())
+    register_queue(app, config, queue_service)
